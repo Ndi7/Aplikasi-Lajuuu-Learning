1c4a22b6c509e8f7d0ae65a29b2c09762e72b8c6
Ndi7 <ndigiawa.7@gmail.com>
2025-04-17 21:05:04 +0700

    Add Firebase Folder Konfigurasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'headerbig_bar.dart';
 import 'bottom_bar.dart';
 import 'halaman_utama.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
